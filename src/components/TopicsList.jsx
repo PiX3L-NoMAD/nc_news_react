3756fbf2f64ba99e6d5564450e86d1c1ee8cd569
Janilee <janileesvardstal@gmail.com>
2025-02-09 21:19:21 +0000

    fixes topics-list display issues

--- src/components/TopicsList.jsx
+++ src/components/TopicsList.jsx
@@ -27,7 +27,7 @@ const TopicsList = () => {
     }, []);
 
     return (
-        <ul className="justify-center grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        <ul className="justify-center grid grid-cols-1 md:grid-cols-3 gap-4">
             {topicsWithImages.map((topic) => (
                 <li key={topic.slug}>
                     <Link to={`/topics/${topic.slug}`}>
