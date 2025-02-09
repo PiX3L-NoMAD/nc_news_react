4bef5a6a4d6e57c9b9bef4f181bbf4e520b64b81
Janilee <janileesvardstal@gmail.com>
2025-02-09 17:54:03 +0000

    adds netlify.toml file and removes _redirect

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html')
+      }
+    }
+  },
+  assetsInclude: ['_redirects'],
   plugins: [react()],
 })
